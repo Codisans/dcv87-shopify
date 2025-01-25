d1b10c7a76009d89e993f1d82ffc5016f04b3b54
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-01-25 03:11:15 +0000

    feat: add main nav

--- app/components/CartLink.jsx
+++ app/components/CartLink.jsx
@@ -1,9 +1,9 @@
 import {Link} from '@remix-run/react';
 import {Symbol} from './Symbol';
 
-export const CartLink = ({cart}) => {
+export const CartLink = ({cart, className = 'relative'}) => {
   return (
-    <Link className="relative" to="/cart">
+    <Link className={`${className}`} to="/cart">
       <span className="sr-only">Cart</span>
       <Symbol className="w-10 h-10" name="cart" />
       <span className="absolute top-0 right-0 rounded-full bg-red text-white text-h4">
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,7 +6,7 @@ import {
   useAsyncValue,
   useLocation,
 } from '@remix-run/react';
-import {useAnalytics, useOptimisticCart} from '@shopify/hydrogen';
+import {Image, useAnalytics, useOptimisticCart} from '@shopify/hydrogen';
 import {useAside} from '~/components/Aside';
 import {Symbol} from './Symbol';
 import {CartLink} from './CartLink';
@@ -18,23 +18,22 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
   const {shop, menu} = header;
 
   return (
-    <header className="sticky top-0 inset-x-0 z-10 bg-white">
-      <div className="container flex flex-row justify-between items-center py-2 gap-4">
-        <div>
-          <NavLink prefetch="intent" to="/" end>
-            <span className="sr-only">{shop.name}</span>
-            La Torre Ambulante
-          </NavLink>
-        </div>
-        <div className="flex flex-row gap-4">
-          <HeaderMenu
-            menu={menu}
-            primaryDomainUrl={header.shop.primaryDomain.url}
-            publicStoreDomain={publicStoreDomain}
-          />
-          <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
-        </div>
+    <header className="fixed inset-0 pointer-events-none hover:before:opacity-100 before:bg-black/80 before:opacity-0 before:transition-opacity before:duration-300 before:absolute before:inset-0 before:left-0 before:-z-1">
+      <div className="absolute top-4 left-1/2 -translate-x-1/2 pointer-events-auto">
+        <NavLink prefetch="intent" to="/" end>
+          <span className="sr-only">{shop.name}</span>
+          <Image className="w-40 h-20" src="/img/dcv-logo.png" alt="DCV Logo" />
+        </NavLink>
       </div>
+      <CartLink
+        className="fixed top-4 right-8 p-2 pointer-events-auto"
+        cart={cart}
+      />
+      <HeaderMenu
+        menu={menu}
+        primaryDomainUrl={header.shop.primaryDomain.url}
+        publicStoreDomain={publicStoreDomain}
+      />
     </header>
   );
 }
@@ -49,7 +48,10 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
  */
 export function HeaderMenu({menu, primaryDomainUrl, publicStoreDomain}) {
   return (
-    <nav className="flex flex-row gap-4" role="navigation">
+    <nav
+      className="fixed top-0 left-0 p-4 flex flex-col text-h2 pointer-events-auto"
+      role="navigation"
+    >
       {(menu || FALLBACK_HEADER_MENU).items.map((item) => {
         if (!item.url) return null;
 
@@ -62,7 +64,7 @@ export function HeaderMenu({menu, primaryDomainUrl, publicStoreDomain}) {
             : item.url;
         return (
           <NavLink
-            className="header-menu-item"
+            className="clip-hover"
             end
             key={item.id}
             prefetch="intent"
