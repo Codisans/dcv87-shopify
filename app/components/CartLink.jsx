9e1a9e08a864af12c2e17be452253acfeb7618a6
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-02-01 01:17:40 +0000

    fix: amend cart icon

--- app/components/CartLink.jsx
+++ app/components/CartLink.jsx
@@ -13,7 +13,9 @@ export function CartLink({cart, className = 'relative'}) {
       <Symbol className="w-10 h-10" name="cart" />
       <span className="absolute top-[-0.2rem] right-[-0.2rem] rounded-full size-8 flex items-center justify-center bg-red text-white text-h4">
         <Suspense fallback={<span>0</span>}>
-          <Await resolve={cart}>{(c) => <span>{c?.totalQuantity}</span>}</Await>
+          <Await resolve={cart}>
+            {(c) => <span>{c?.totalQuantity || 0}</span>}
+          </Await>
         </Suspense>
       </span>
     </Link>
