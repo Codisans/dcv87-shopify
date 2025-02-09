08a5f53bc485f3f7f559c8b2647bf38f22f212c5
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-02-09 15:23:15 +0000

    save wip

--- app/components/WeatherWidget.jsx
+++ app/components/WeatherWidget.jsx
@@ -2,7 +2,7 @@ import {Await} from '@remix-run/react';
 import {Suspense} from 'react';
 
 export const WeatherWidget = ({ip}) => {
-  const location = fetch(`https://ipapi.co/${ip}/json/`)
+  const location = fetch(`https://ipapi.co/${ip}/json/`, {mode: 'no-cors'})
     .then((res) => res.json())
     .catch((err) => console.log(err));
 
