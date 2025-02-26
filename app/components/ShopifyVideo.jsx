d08b30609b762a2870c1489074b7c689dc007913
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-02-26 19:33:43 +0000

    fix: amend missing muted attr on video

--- app/components/ShopifyVideo.jsx
+++ app/components/ShopifyVideo.jsx
@@ -1,10 +1,20 @@
 import {Video} from '@shopify/hydrogen';
+import {useEffect, useRef} from 'react';
 
 export const ShopifyVideo = ({video, className = 'w-full', ...props}) => {
+  const videoRef = useRef(null);
+
   if (!video) return null;
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+
+    videoRef.current.setAttribute('muted', 'true');
+  }, [videoRef]);
 
   return (
     <Video
+      ref={videoRef}
       className={className}
       data={video}
       {...props}
