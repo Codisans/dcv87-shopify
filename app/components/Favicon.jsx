08260f32652f3110f4f79a7ede01d8fcf01e12e8
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-02-26 22:13:50 +0000

    fix: amend favicon

--- app/components/Favicon.jsx
+++ app/components/Favicon.jsx
@@ -1,21 +1,37 @@
-export const Favicon = () => {
-  return (
-    <>
-      <link
-        rel="icon"
-        type="image/png"
-        href="/favicon-96x96.png"
-        sizes="96x96"
-      />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <meta name="apple-mobile-web-app-title" content="DCV'87" />
-      <link rel="manifest" href="/site.webmanifest" />
-    </>
-  );
+export const FaviconLinks = () => {
+  return [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon-96x96.png',
+      sizes: '96x96',
+    },
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    },
+    {
+      rel: 'shortcut icon',
+      href: '/favicon.ico',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+  ];
+};
+
+export const FaviconMeta = () => {
+  return [
+    {
+      name: 'apple-mobile-web-app-title',
+      content: "DCV'87",
+    },
+  ];
 };
