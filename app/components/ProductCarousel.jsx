6d0cacf1e306f211c5dd1651f27c050b21e121c3
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-03-01 14:44:01 +0000

    fix: refctor carousel

--- app/components/ProductCarousel.jsx
+++ app/components/ProductCarousel.jsx
@@ -5,29 +5,33 @@ import gsap from 'gsap';
 import {useVariantUrl} from '~/lib/variants';
 import {parseFields} from '~/utils/parseFields';
 import {ProductImage} from '~/components/ProductImage';
+import {useGSAP} from '@gsap/react';
+
+gsap.registerPlugin(useGSAP);
 
 export const ProductCarousel = ({collection, products}) => {
   const {pathname} = useLocation();
+  const navigate = useNavigate();
+
+  const [renderedItems, setRenderedItems] = useState([]);
 
   const dragInterfaceRef = useRef(null);
   const carouselRef = useRef(null);
   const groupRef = useRef(null);
-  const itemsArrayRef = useRef([]);
+  const placeholderRef = useRef(null);
   const tweenRef = useRef(null);
+  const prevX = useRef(null);
+  const currentDirection = useRef(1);
+  const resizeObserverRef = useRef(null);
   const [tickerDimensions, setTickerDimensions] = useState(null);
 
-  const addItemRef = useCallback((node) => {
-    if (node && !itemsArrayRef.current.includes(node)) {
-      itemsArrayRef.current.push(node);
-    }
-  }, []);
-
   const getTickerDimensions = () => {
-    const visibleWidth = groupRef.current?.clientWidth;
-    const itemWidth = itemsArrayRef.current[0]?.clientWidth;
-    const totalWidth = itemWidth * itemsArrayRef.current.length;
+    const visibleWidth = carouselRef.current?.clientWidth;
+    const itemWidth = placeholderRef.current?.clientWidth;
+    const totalWidth = itemWidth * products.length;
     const extraWidth = visibleWidth + Math.max(visibleWidth - totalWidth, 0);
     const extraItems = Math.ceil(extraWidth / itemWidth);
+
     return {
       visibleWidth,
       itemWidth,
@@ -37,138 +41,142 @@ export const ProductCarousel = ({collection, products}) => {
     };
   };
 
-  let prevX = null;
-  let currentDirection = 1;
-
   useEffect(() => {
-    if (!groupRef.current || !itemsArrayRef.current.length) {
+    if (!groupRef.current || !placeholderRef.current) {
       return;
     }
 
     setTickerDimensions(getTickerDimensions());
-    const resizeObserver = new ResizeObserver(() => {
-      setTickerDimensions(getTickerDimensions());
-    });
-    resizeObserver.observe(groupRef.current);
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [groupRef.current, itemsArrayRef.current]);
+  }, [groupRef, placeholderRef]);
 
   useEffect(() => {
-    if (groupRef.current) {
-      gsap.set(groupRef.current, {clearProps: 'all'});
-    }
-
-    tweenRef.current?.kill();
-
-    // carouselRef.current?.removeEventListener('mouseenter', handleMouseEnter);
-    // carouselRef.current?.removeEventListener('mouseleave', handleMouseLeave);
-    // dragInterfaceRef.current?.removeEventListener('drag', handleMouseDrag);
-    // dragInterfaceRef.current?.removeEventListener('dragend', handleDragEnd);
-    // dragInterfaceRef.current?.removeEventListener(
-    //   'touchmove',
-    //   handleTouchMove,
-    // );
-    // dragInterfaceRef.current?.removeEventListener('touchend', handleDragEnd);
-
-    if (!tickerDimensions) {
+    if (!groupRef.current || !placeholderRef.current) {
       return;
     }
 
-    const {totalWidth} = tickerDimensions;
+    resizeObserverRef.current = new ResizeObserver(() => {
+      setTickerDimensions(getTickerDimensions());
+    });
+    resizeObserverRef.current.observe(carouselRef.current);
 
-    if (tweenRef.current) {
-      tweenRef.current.kill();
+    return () => {
+      resizeObserverRef.current?.disconnect();
+    };
+  }, [groupRef.current, placeholderRef.current]);
+
+  const {contextSafe, context} = useGSAP(
+    (context) => {
+      context.kill();
+      if (!tickerDimensions) {
+        return;
+      }
+
+      const {totalWidth} = tickerDimensions;
+
+      tweenRef.current = gsap.to(groupRef.current, {
+        translateX: `-=${totalWidth}`,
+        duration: totalWidth / 180,
+        ease: 'none',
+        repeat: -1,
+        onReverseComplete: () => {
+          tweenRef.current.totalTime(
+            tweenRef.current.totalTime() + tweenRef.current.duration() * 100,
+            true,
+          );
+        },
+      });
+
+      return () => {
+        tweenRef.current = null;
+        context.kill();
+        prevX.current = null;
+      };
+    },
+    {dependencies: [renderedItems]},
+  );
+
+  const stopTween = contextSafe(() => {
+    tweenRef.current?.kill();
+  });
+
+  const manipulateTween = contextSafe(({timeScale}) => {
+    gsap.to(tweenRef.current, {
+      timeScale: timeScale,
+    });
+  });
+
+  const pauseTween = () => manipulateTween({timeScale: 0});
+
+  const playTween = () => {
+    prevX.current = null;
+    manipulateTween({timeScale: currentDirection.current});
+  };
+
+  const handleDrag = (currentX) => {
+    if (currentX == 0 || prevX.current === currentX) return;
+
+    if (!prevX.current) {
+      prevX.current = currentX;
+      return;
     }
 
-    tweenRef.current = gsap.to(groupRef.current, {
-      translateX: `-=${totalWidth}`,
-      duration: totalWidth / 180,
-      ease: 'none',
-      repeat: -1,
-      onReverseComplete: () => {
-        console.log('onReverseComplete');
-        tweenRef.current.totalTime(
-          tweenRef.current.totalTime() + tweenRef.current.duration() * 100,
-          true,
-        );
-      },
-    });
+    const delta = prevX.current - currentX;
+    currentDirection.current = delta > 0 ? 1 : -1;
+    const sensitivity = 2;
+    const timeScale =
+      delta === 0 ? currentDirection.current : delta * sensitivity;
+    manipulateTween({timeScale: timeScale});
+    prevX.current = currentX;
+  };
+
+  const handleMouseEnter = () => {
+    pauseTween();
+  };
+
+  const handleMouseLeave = () => {
+    playTween();
+  };
+
+  const handleMouseDrag = (e) => {
+    handleDrag(e.pageX);
+  };
+
+  const handleMouseDragEnd = () => {
+    playTween();
+  };
+
+  const handleTouchEnd = () => {
+    playTween();
+  };
+
+  const handleTouchMove = (e) => {
+    handleDrag(e.touches[0]?.clientX);
+  };
+
+  useEffect(() => {
+    if (!dragInterfaceRef.current || !carouselRef.current) return;
 
     const hasMouse = window.matchMedia(
       '(pointer: fine) and (hover: hover)',
     ).matches;
 
-    const updateTween = (delta) => {
-      const velocity = Math.abs(delta);
-
-      if (velocity === 0) {
-        return;
-      }
-
-      const direction = delta === 0 ? currentDirection : delta < 0 ? 1 : -1;
-      currentDirection = direction;
-
-      if (velocity > 0) {
-        gsap.to(tweenRef.current, {
-          timeScale: velocity * direction * 1.4,
-        });
-      }
-    };
-    const handleDrag = (e, x) => {
-      if (x == 0) return;
-      if (!prevX) {
-        prevX = x;
-        return;
-      }
-      updateTween(x - prevX);
-      prevX = x;
-    };
-    const handleMouseEnter = () => {
-      gsap.to(tweenRef.current, {
-        timeScale: 0,
-      });
-    };
-    const handleMouseLeave = () => {
-      gsap.to(tweenRef.current, {
-        timeScale: currentDirection,
-      });
-    };
-    const handleMouseDrag = (e) => {
-      handleDrag(e, e.pageX);
-    };
-    const handleDragEnd = () => {
-      prevX = null;
-      gsap.to(tweenRef.current, {
-        timeScale: currentDirection,
-      });
-    };
-    const handleTouchEnd = () => {
-      prevX = null;
-      gsap.to(tweenRef.current, {
-        timeScale: currentDirection,
-      });
-    };
-
-    const handleTouchMove = (e) => {
-      handleDrag(e, e.touches[0]?.clientX);
-    };
-
     if (hasMouse) {
-      dragInterfaceRef.current?.addEventListener('drag', handleMouseDrag, {
-        passive: false,
-      });
-      dragInterfaceRef.current?.addEventListener('dragend', handleDragEnd, {
-        passive: false,
-      });
       carouselRef.current?.addEventListener('mouseenter', handleMouseEnter, {
         passive: false,
       });
       carouselRef.current?.addEventListener('mouseleave', handleMouseLeave, {
         passive: false,
       });
+      dragInterfaceRef.current?.addEventListener('drag', handleMouseDrag, {
+        passive: false,
+      });
+      dragInterfaceRef.current?.addEventListener(
+        'dragend',
+        handleMouseDragEnd,
+        {
+          passive: false,
+        },
+      );
     } else {
       dragInterfaceRef.current?.addEventListener('touchmove', handleTouchMove, {
         passive: false,
@@ -179,48 +187,58 @@ export const ProductCarousel = ({collection, products}) => {
     }
 
     return () => {
-      if (groupRef.current) {
-        gsap.set(groupRef.current, {clearProps: 'all'});
-      }
-
-      tweenRef.current?.kill();
-
       carouselRef.current?.removeEventListener('mouseenter', handleMouseEnter);
       carouselRef.current?.removeEventListener('mouseleave', handleMouseLeave);
       dragInterfaceRef.current?.removeEventListener('drag', handleMouseDrag);
-      dragInterfaceRef.current?.removeEventListener('dragend', handleDragEnd);
+      dragInterfaceRef.current?.removeEventListener(
+        'dragend',
+        handleMouseDragEnd,
+      );
       dragInterfaceRef.current?.removeEventListener(
         'touchmove',
         handleTouchMove,
       );
-      dragInterfaceRef.current?.removeEventListener('touchend', handleDragEnd);
+      dragInterfaceRef.current?.removeEventListener('touchend', handleTouchEnd);
     };
-  }, [tickerDimensions]);
+  }, [dragInterfaceRef, carouselRef]);
 
-  const carouselItems = products.concat(
-    Array.from(
-      {length: tickerDimensions?.extraItems || 0},
-      (_, index) => products[index % products.length],
-    ),
-  );
+  const navigateToProduct = contextSafe((target, url) => {
+    tweenRef.current?.kill();
+    const rect = target.getBoundingClientRect();
+    const centerOffset =
+      window.innerWidth / 2 - rect.left - target.offsetWidth / 2;
+
+    target.parentElement.classList.add('in-view');
+    carouselRef.current?.classList.remove('in-view');
+
+    gsap
+      .to(carouselRef.current, {
+        x: centerOffset,
+        duration: 0.3,
+        ease: 'power2.inOut',
+      })
+      .then(() => navigate(url));
+  });
 
   return (
     <div
       ref={dragInterfaceRef}
-      className="relative h-svh z-10 flex flex-col justify-center py-12 lg:py-20"
+      className="relative h-svh z-10 flex flex-col justify-center py-12 lg:py-20 in-view:opacity-100 opacity-0 transition-opacity duration-300 ease-slide"
     >
       <div ref={carouselRef} className="w-full overflow-hidden in-view">
         <ul ref={groupRef} className="flex w-full items-center">
-          {carouselItems?.map((product, i) => (
+          <div
+            ref={placeholderRef}
+            className="w-[16rem] sm:w-[20rem] lg:w-[24rem] absolute pointer-events-none invisible"
+          ></div>
+          {renderedItems?.map((product, i) => (
             <li
-              ref={i < products.length ? addItemRef : undefined}
               key={`${collection.title}-${product.id}-${i}`}
               className="flex-none"
             >
               <ProductItem
                 product={product}
-                tweenRef={tweenRef}
-                carouselRef={carouselRef}
+                navigateToProduct={navigateToProduct}
                 loading={i < 8 ? 'eager' : undefined}
               />
             </li>
@@ -237,10 +255,9 @@ export const ProductCarousel = ({collection, products}) => {
  *   loading?: 'eager' | 'lazy';
  * }}
  */
-function ProductItem({product, loading, carouselRef, tweenRef}) {
+function ProductItem({product, loading, navigateToProduct}) {
   const variantUrl = useVariantUrl(product.handle);
   const mediaRef = useRef(null);
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (!product || mediaRef.current) return;
@@ -251,22 +268,7 @@ function ProductItem({product, loading, carouselRef, tweenRef}) {
   const handleClick = (e) => {
     e.preventDefault();
     mediaRef.current?.classList.add('!opacity-100');
-    const rect = e.target.getBoundingClientRect();
-    const centerOffset =
-      window.innerWidth / 2 - rect.left - e.target.offsetWidth / 2;
-    tweenRef.current.pause();
-
-    window.requestAnimationFrame(() => {
-      e.target.parentElement.classList.add('in-view');
-      carouselRef.current?.classList.remove('in-view');
-      gsap
-        .to(carouselRef.current, {
-          x: centerOffset,
-          duration: 0.3,
-          ease: 'power2.inOut',
-        })
-        .then(() => navigate(variantUrl));
-    });
+    navigateToProduct(e.target, variantUrl);
   };
 
   return (
